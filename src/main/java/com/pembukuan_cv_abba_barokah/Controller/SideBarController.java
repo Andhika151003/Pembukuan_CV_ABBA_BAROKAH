26955507366a429835d688033952f80380b5dd0f
Cellicel <arrac56@gmail.com>
2026-01-29 21:24:34 +0700

    update besar2 an

--- src/main/java/com/pembukuan_cv_abba_barokah/Controller/SideBarController.java
+++ src/main/java/com/pembukuan_cv_abba_barokah/Controller/SideBarController.java
@@ -33,6 +33,16 @@ public class SideBarController {
         loadContent("/com/pembukuan_cv_abba_barokah/View/BiayaAdministrasiMain.fxml");
     }
 
+    @FXML
+    private void openModal() {
+        loadContent("/com/pembukuan_cv_abba_barokah/View/Modal.fxml");
+    }
+
+    @FXML
+    private void openPenjualan() {
+        loadContent("/com/pembukuan_cv_abba_barokah/View/Penjualan.fxml");
+    }
+
     private void loadContent(String fxml) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(fxml));
